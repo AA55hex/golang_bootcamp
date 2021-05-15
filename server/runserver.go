579ffb26697b6dd93de41a99842a8045ccc49b59
AA55hex <roma.gupalo2015@yandex.ru>
2021-05-15 19:50:01 +0300

    Session connection -> session package

--- server/runserver.go
+++ server/runserver.go
@@ -1,31 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/upper/db/v4/adapter/mysql"
+	"github.com/AA55hex/golang_bootcamp/server/db/session"
 )
 
-var db_settings mysql.ConnectionURL
-
 func main() {
-	// create database connection
-	db_settings = mysql.ConnectionURL{
-		Database: `book_store`,
-		Host:     `mysql_docker`,
-		User:     `root`,
-		Password: `pseudo_pass`,
-	}
-	// open db session
-	fmt.Println("Open session: ", db_settings)
-	session, err := mysql.Open(db_settings)
+	// create database session
+	con, err := session.Create()
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	defer session.Close()
-	fmt.Println("Session created")
+	defer con.Close()
 	// execute migration scripts
 	// ...
 	// get router
