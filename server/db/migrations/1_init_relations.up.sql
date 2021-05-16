778890551c0d946fbaaa36630f3f05d69b9ed678
AA55hex <roma.gupalo2015@yandex.ru>
2021-05-16 19:34:29 +0300

    Added unique flag to name columns

--- server/db/migrations/1_init_relations.up.sql
+++ server/db/migrations/1_init_relations.up.sql
@@ -1,14 +1,14 @@
 create table if not exists genre
 (
     id int not null,
-    `name` varchar(20) not null,
+    `name` varchar(20) unique not null,
     primary key (id)
 );
 
 create table if not exists book
 (
     id int auto_increment not null,
-    `name` varchar(100) not null,
+    `name` varchar(100) unique not null,
     price float default 0 not null,
     genre int not null,
     amount int default 0 not null,
