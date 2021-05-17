c2eed46caf9c9cc7f2e8aae1033c468950574a95
AA55hex <roma.gupalo2015@yandex.ru>
2021-05-17 17:35:44 +0300

    Added server listening

--- server/runserver.go
+++ server/runserver.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/AA55hex/golang_bootcamp/server/db/connection"
+	"github.com/AA55hex/golang_bootcamp/server/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	// create database session
 	if connection.GetSession() == nil {
-		return
+		log.Fatal("Database session not created")
 	}
 	defer connection.GetSession().Close()
-	// create router
 
+	// create router
+	fmt.Println("Creating router")
+	router := mux.NewRouter()
+	router.HandleFunc("/books/{id:[0-9]+}", handlers.GetBookByIdHandler)
 	// listen & serve
+	fmt.Println("Creating server")
+	server := http.Server{
+		Handler:      router,
+		Addr:         ":3000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	fmt.Println("Server created.")
+	fmt.Println("Listening started on", server.Addr)
+	log.Fatal(server.ListenAndServe())
 }
