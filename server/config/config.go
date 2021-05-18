a8c77104079ad419ca9e2bd953dfb740c7fc3d87
AA55hex <roma.gupalo2015@yandex.ru>
2021-05-18 17:08:15 +0300

    ENV parsing updated

--- server/config/config.go
+++ server/config/config.go
@@ -23,12 +23,31 @@ var Server struct {
 }
 
 // simple func for setting env variable into string variable
-func set_env(env_var *string, env_string string, default_string string) {
+func set_env_string(env_var *string, env_string string, default_string string) {
 	var ok bool
 	*env_var, ok = os.LookupEnv(env_string)
 	if !ok {
 		*env_var = default_string
 	}
+}
+
+// simple func for setting env variable into int variable
+func set_env_int(env_var *int, env_string string, default_int int) {
+	str, ok := os.LookupEnv(env_string)
+	buff, err := strconv.ParseInt(str, 10, 32)
+	switch {
+	case !ok:
+		*env_var = default_int
+		fmt.Println(env_var, ": set to default.")
+		return
+	case err != nil:
+		*env_var = default_int
+		fmt.Println("Bad ", env_var, ": ", buff, ". Set to default.")
+	default:
+		*env_var = int(buff)
+		fmt.Println(env_var, ": ", buff)
+	}
+	return
 }
 
 // Init all env variables from configs.env
@@ -38,17 +57,12 @@ func init() {
 		log.Println("No .env file found: ", err)
 	}
 
-	set_env(&MySQL.Host, "MYSQL_HOST", "localhost:3306")
-	set_env(&MySQL.Database, "MYSQL_DB", "")
-	set_env(&MySQL.User, "MYSQL_USER", "root")
-	set_env(&MySQL.Password, "MYSQL_PASSOWRD", "")
-	set_env(&MySQL.MigrationSource, "MIGRATION_SOURCE", "file:///migrations/")
+	set_env_string(&MySQL.Host, "MYSQL_HOST", "localhost:3306")
+	set_env_string(&MySQL.Database, "MYSQL_DB", "")
+	set_env_string(&MySQL.User, "MYSQL_USER", "root")
+	set_env_string(&MySQL.Password, "MYSQL_PASSOWRD", "")
+	set_env_string(&MySQL.MigrationSource, "MIGRATION_SOURCE", "file:///migrations/")
+	set_env_string(&Server.Address, "SERVER_ADDRESS", "localhost:8000")
 
-	set_env(&Server.Address, "SERVER_ADDRESS", "localhost:8000")
-
-	try_count, _ := os.LookupEnv("CONNECTION_TRY_COUNT")
-	ConnectionTryCount, err := strconv.ParseInt(try_count, 10, 32)
-	if err != nil || ConnectionTryCount <= 0 {
-		ConnectionTryCount = 30
-	}
+	set_env_int(&MySQL.ConnectionTryCount, "CONNECTION_TRY_COUNT", 50)
 }
