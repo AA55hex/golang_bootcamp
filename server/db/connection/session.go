96f353ddafcf00ac826d968601ef7ce64c3cb34f
AA55hex <roma.gupalo2015@yandex.ru>
2021-05-18 12:20:54 +0300

    Added trials for connection

--- server/db/connection/session.go
+++ server/db/connection/session.go
@@ -3,6 +3,7 @@ package connection
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/upper/db/v4"
 	"github.com/upper/db/v4/adapter/mysql"
@@ -22,20 +23,31 @@ func init() {
 			"multiStatements": "true",
 		},
 	}
-
-	// open db session
-	fmt.Println("Try open session: ", db_settings)
 	var err error
-	session, err = mysql.Open(db_settings)
-	if err != nil {
-		fmt.Print(err)
-	}
-	fmt.Println("Session created")
+	// open db session
+	fmt.Println("Connection: ", db_settings)
+	for i := 0; i < 30; i++ {
+		fmt.Print("Try open session ", i, ": ")
 
+		session, err = mysql.Open(db_settings)
+		if err != nil {
+			fmt.Println("FAIL.")
+			fmt.Println("Error", err)
+			time.Sleep(3 * time.Second)
+		} else {
+			fmt.Println("SUCCESS!")
+			break
+		}
+	}
+	if session == nil {
+		return
+	}
 	// try migrate
 	internalSQLDriver := session.Driver().(*sql.DB)
 	err = try_migrate(internalSQLDriver)
 	if err != nil {
+		session.Close()
+		session = nil
 		fmt.Print(err)
 	}
 }
