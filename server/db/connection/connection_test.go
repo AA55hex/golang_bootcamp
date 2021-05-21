8e96560e3e50d66e628a477fb20c939ba11e1f35
AA55hex <roma.gupalo2015@yandex.ru>
2021-05-21 22:17:39 +0300

    Tests fixed

--- server/db/connection/connection_test.go
+++ server/db/connection/connection_test.go
@@ -5,52 +5,63 @@ import (
 	"testing"
 
 	"github.com/AA55hex/golang_bootcamp/server/config"
-	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 	"github.com/upper/db/v4"
 	"github.com/upper/db/v4/adapter/mysql"
 )
 
+func init() {
+	config.LoadConfigs("../../configs.env")
+	db_settings = &mysql.ConnectionURL{
+		Database: config.MySQL.Database,
+		Host:     config.MySQL.Host,
+		User:     config.MySQL.User,
+		Password: config.MySQL.Password,
+	}
+}
+
 type mocked_adapter struct {
-	mock.Mock
 	isFailTest bool
 }
 
 var fail_error = errors.New("")
 
 func (m *mocked_adapter) Open(connURL db.ConnectionURL) (db.Session, error) {
-	m.Called(connURL)
 	if m.isFailTest {
 		return nil, fail_error
 	}
-	return nil, nil
-
+	sess, err := mysql.Open(connURL)
+	return sess, err
 }
 
-var db_settings = &mysql.ConnectionURL{
-	Database: config.MySQL.Database,
-	Host:     config.MySQL.Host,
-	User:     config.MySQL.User,
-	Password: config.MySQL.Password,
-}
+var db_settings *mysql.ConnectionURL
 
 func TestOpenSessionOnSuccess(t *testing.T) {
 	adp := new(mocked_adapter)
 	adapter = adp
 
-	adp.On("Open", db_settings).Return(nil, nil)
-
-	OpenSession(db_settings, 1)
-
-	adp.AssertExpectations(t)
+	sess, err := OpenSession(db_settings, 1)
+	defer func() {
+		if session != nil {
+			sess.Close()
+			session = nil
+		}
+	}()
+	require.NoError(t, err, "Unexpected error: %v", err)
+	require.NotNil(t, sess, "Unexpected nil in session variable.")
 }
 
 func TestOpenSessionOnFail(t *testing.T) {
 	adp := &mocked_adapter{isFailTest: true}
 	adapter = adp
 
-	adp.On("Open", db_settings).Return(nil, fail_error)
-
-	OpenSession(db_settings, 2)
-
-	adp.AssertExpectations(t)
+	sess, err := OpenSession(db_settings, 2)
+	defer func() {
+		if session != nil {
+			sess.Close()
+			session = nil
+		}
+	}()
+	require.Error(t, err, "Expected error.")
+	require.Nil(t, sess, "Unexpected data in session variable.")
 }
